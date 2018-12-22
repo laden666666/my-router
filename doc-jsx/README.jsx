21c0db521e79b31bf46ffdfbbb665f92c0890ad4
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2018-12-22 23:20:37 +0800

    更新到主文档中

--- doc-jsx/README.jsx
+++ doc-jsx/README.jsx
@@ -49,4 +49,7 @@
 
     <h2>my-router</h2>
     <p>my-router设计的目的就是寻找一套webapp和app一样的路由系统，my-router试图进一步抽象history对象和location对象，将其封装为一个类似app路由的，不再提供history对象和location对象，而是进行更进一步的路由抽象。从而使得我们的webapp表现形式更像app。</p>
+
+    <h2><a href="./packages/my-router-history">my-router-history</a></h2>
+    <p>一个屏蔽了浏览器地址栏的诸多特性的history库，用于开发高仿APP的WEB路由。浏览器的地址栏可以修改URL，可以访问历史记录，无法监听返回事件等特性，这些特性都是APP不具备的。<strong>my-router-history</strong>致力于屏蔽这些特性，提供一个开放高仿APP路由的可能性。该项目并不依赖<strong>my-router</strong>，因此如果你感兴趣开发一个APP式的路由，可以直接参考甚至使用该项目。</p>
 </doc>
