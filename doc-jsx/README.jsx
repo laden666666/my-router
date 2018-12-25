6d22378b6723957243914d70b4ab1d305693bb2a
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2018-12-25 18:10:42 +0800

    修改文档

--- doc-jsx/README.jsx
+++ doc-jsx/README.jsx
@@ -1,12 +1,15 @@
 <doc>
     <h1>my-router</h1>
-    <p>在服务器端，路由是根据不同的Url路由，拼接出对应的视图页面技术。前后分离后，路由概念慢慢渗透到客户端，现在连Reac Native、flutter、微信小程序等App框架也引入了路由的概念。</p>
+    <p>在服务器端，路由是根据不同的Url路由，拼接出对应的视图页面技术。前后分离后，路由概念慢慢渗透到前端，现在连Reac Native、flutter、微信小程序等App框架也引入了路由的概念。</p>
 
     <h2>更接近App的WebApp路由系统——my-router</h2>
-    <p>目前主流的WebApp路由，都是基于浏览器的地址栏开发的。因为浏览器地址栏将URL暴露给用户，WebApp的路由无法限制（引导）用户的工作流，也无法监听移动设备的返回键，甚至无法阻止用户从缓存的历史记录中跳来跳去。</p>
+    <p>目前主流的WebApp路由，都是基于浏览器的地址栏开发的。因为浏览器地址栏将URL暴露给用户，WebApp的路由无法限制（引导）用户的工作流，也无法监听移动设备的返回键。</p>
 
-    <img src="./location.png" alt=""/>
-    <img src="./cache.png" alt=""/>
+    <li>用户可以后退、前进、刷新页面、修改URL</li>
+    <img src="./doc-jsx/location.png" alt=""/>
+
+    <li>用户可以从缓存的历史记录中跳来跳去</li>
+    <img src="./doc-jsx/cache.png" alt=""/>
 
     
     <p><strong>my-router</strong>设计的目的就是寻找一套接近App的WebApp路由系统，我们试图进一步抽象history对象和location对象，屏蔽了浏览器地址栏的一些鸡肋而且容易出现漏洞的功能，限制了用户的一部分自由，但是提高了用户体验，使用户享受到更接近原生APP的的路由系统。而对于程序员，my-router会提供更全面的API，同时让开发者不再受浏览器的束缚，让大家享受路由开发工作。</p>
@@ -46,7 +49,7 @@
         <tr>
             <td>返回按钮</td>
             <td>有</td>
-            <td>安卓可以，IOS没有</td>
+            <td>安卓有，IOS没有</td>
             <td>有</td>
         </tr>
         <tr>
@@ -58,15 +61,15 @@
         <tr>
             <td>用户可以刷新</td>
             <td>可以</td>
-            <td>不可用</td>
+            <td>不能</td>
             <td>可以</td>
         </tr>
     </table>
 
     <h2><a href="./packages/my-router-history">my-router-history</a></h2>
-    <p>一个屏蔽了浏览器地址栏的诸多特性的history库，用于实现在浏览器上高仿APP式的单页面路由。<strong>my-reoute-history</strong>屏蔽了浏览器记录用户历史记录的行为，劫持用户了用户的返回动作，用于开发仿APP交换的SAP</p>
-    <p>该项目并不依赖<strong>my-router</strong>，因此如果你感兴趣开发一个APP式的路由，可以直接参考甚至使用<strong>my-reoute-history</strong>。</p>
+    <p>旨在用于浏览器上实现高仿APP式的单页面路由。<strong>my-reoute-history</strong>屏蔽了浏览器的诸多行为：记录用户历史记录、前进按钮等，并监听用户返回动作，使得WebApp路由变得更像App。可用于PWA路由开发、WebApp路由开发、微信小程序路由移植等场景。</p>
+    <p>该项目并不依赖<strong>my-router</strong>，因此如果你感兴趣开发一个仿App的路由，可以直接参考甚至使用<strong>my-reoute-history</strong>。</p>
 
-    <h2><a href="./packages/my-router-history">my-router</a></h2>
-    <p>一个基于异步的路由系统，my-router提供了路由会话概念，让用户路由跳转开发变得更容易。</p>
+    <h2>my-router</h2>
+    <p>一个基于异步的路由系统，my-router提供了路由会话概念，让路由跳转开发变得更容易。</p>
 </doc>
--- packages/my-router-history/doc-jsx/README.jsx
+++ packages/my-router-history/doc-jsx/README.jsx
@@ -1,32 +1,34 @@
 <doc>
     <h1>my-reoute-history</h1>
 
-    <p><strong>my-reoute-history</strong>是<a href="../../">my-reoute</a>的一个子项目，旨在用于浏览器上实现高仿APP式的单页面路由。<strong>my-reoute-history</strong>屏蔽了浏览器的诸多行为：记录用户历史记录、前进按钮等，并监听用户返回动作，使得WebApp路由变得更像App。可用于PWA路由开发、WebApp路由开发、微信小程序路由移植等场景。</p>
+    <p><strong>my-reoute-history</strong>是<a href="https://github.com/laden666666">my-reoute</a>的一个子项目，旨在用于浏览器上实现高仿APP式的单页面路由。<strong>my-reoute-history</strong>屏蔽了浏览器的诸多行为：记录用户历史记录、前进按钮等，并监听用户返回动作，使得WebApp路由变得更像App。可用于PWA路由开发、WebApp路由开发、微信小程序路由移植等场景。</p>
 
     <h2>主要功能</h2>
 
     <p><strong>my-reoute-history</strong>的主要功能如下：</p>
     <h3>增加返回页面</h3>
-    <p><strong>my-reoute-history</strong>会在用户进入Web页面后，插入一个专门监听返回行为的页面。如下图用户进入网页后后，会自动地增加一条名为“返回”的页面。</p>
-    <img src="./GIF.gif" alt=""/>
+    <p><strong>my-reoute-history</strong>会在用户进入Web页面后，插入一个专门监听返回行为的页面。如下图用户进入网页后后，会自动地增加一条名为“返回”的浏览记录。</p>
+    <img src="./doc-jsx/GIF.gif" alt=""/>
 
     <h3>用户跳转页面后，不产生浏览记录</h3>
-    <p>浏览器的地址栏会收集用户跳转页面的记录，<strong>my-reoute-history</strong>屏蔽了这一行为，这主要是阻止防止用户跳转到历史记录的某个页面，因为App是没有这种功能的，App会试图用自己的导航去引导用户，而历史记录会让用户跳出App精心设计的导航，使得工作流变动混乱，用户体验变得更差。</p>
+    <p>浏览器的地址栏会收集用户的浏览记录，<strong>my-reoute-history</strong>屏蔽了这一行为。因为App会试图用自己的导航去引导用户，而历史记录会让用户跳出App精心设计的导航，使得工作流变动混乱，用户体验变得更差。</p>
     
     <p>如下图，用户在地址栏输入地址“/demo”后，页面跳转，但是浏览器中仅存在“返回”一条记录，看不到“首页”记录。</p>
-    <img src="./GIF2.gif" alt=""/>
+    <img src="./doc-jsx/GIF2.gif" alt=""/>
 
-    <h3>返回上一页，地址栏的前进按钮不可用</h3>
-    <p>浏览器中，用户返回上一页，地址栏的“前进按钮”就会变为可点击状态，用户点击前进按钮会回到之前的页面。而<strong>my-reoute-history</strong>屏蔽了这一行为，因为“前进按钮”在App中也是不多见的。如下图，用户点击返回后，前进按钮仍然是不可用状态。</p>
-    <img src="./GIF3.gif" alt=""/>
+    <h3>禁用地址栏的前进按钮</h3>
+    <p>浏览器中，用户返回上一页后，地址栏的“前进按钮”就会变为可点击状态，用户点击“前进按钮”会回到之前的页面。而<strong>my-reoute-history</strong>屏蔽了这一行为，因为“前进按钮”在App中也是没有的。如下图，用户点击返回后，前进按钮仍然是不可用状态。</p>
+
+    <p>如下图，用户后退后，前进页面仍不可点。</p>
+    <img src="./doc-jsx/GIF3.gif" alt=""/>
 
     <h2>其他功能</h2>
     <h3>虚拟历史记录</h3>
-    <p>浏览器地址栏的用户访问的历史记录是开发者不可访问的，<strong>my-reoute-history</strong>还提供了一个虚拟历史记录的功能，让开发者可以访问虚拟的历史记录，并动态的删除、增加用户的访问记录。</p>
+    <p>浏览器地址栏的历史记录是开发者不可访问的，<strong>my-reoute-history</strong>提供了一个虚拟历史记录的功能，用于代替history的历史记录。开发者可以访问虚拟历史记录，并动态的删除、增加用户的访问记录。</p>
     
     <h3>禁止用户修改URL</h3>
     <p>在浏览器中，用户可以修改URL，<strong>my-reoute-history</strong>可以屏蔽这一行为，当用户修改URL后，会用当前页面的URL覆盖掉用户修改的URL。</p>
-    <p>虽然<strong>my-reoute-history</strong>可以屏蔽用户修改URL，但是<strong>my-reoute-history却不建议开发者开启这项功能</strong>，因为我们毕竟是在浏览器中，禁止修改URL，会降低用户的使用体验，这也违反<strong>my-reoute-history</strong>设计的初衷。</p>
+    <p>虽然<strong>my-reoute-history</strong>可以屏蔽用户修改URL，但是<strong>my-reoute-history却不建议开发者开启这项功能</strong>，因为我们毕竟是在浏览器中，禁止修改URL，会降低用户的使用体验，这也违背了<strong>my-reoute-history</strong>设计的初衷。</p>
 
     <h2>原理</h2>
     <p><strong>my-reoute-history</strong>实现原理是返回键劫持，当用户打开页面时候，my-reoute-history会动态插入一个返回键劫持页面，用于监听用户的返回动作。同时使用hashchange事件监听url的hash改变。无论用户前进、后退都会先返回到返回页面，然后再由返回页面进行前进、后台、替换的行为。</p>
