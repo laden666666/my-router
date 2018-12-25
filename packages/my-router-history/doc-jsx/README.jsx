d0a7b7f273ea7486c0b62887852600f10f8d2955
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2018-12-25 18:53:23 +0800

    修改文档

--- packages/my-router-history/doc-jsx/README.jsx
+++ packages/my-router-history/doc-jsx/README.jsx
@@ -1,7 +1,7 @@
 <doc>
     <h1>my-reoute-history</h1>
 
-    <p><strong>my-reoute-history</strong>是<a href="https://github.com/laden666666">my-reoute</a>的一个子项目，旨在用于浏览器上实现高仿APP式的单页面路由。<strong>my-reoute-history</strong>屏蔽了浏览器的诸多行为：记录用户历史记录、前进按钮等，并监听用户返回动作，使得WebApp路由变得更像App。可用于PWA路由开发、WebApp路由开发、微信小程序路由移植等场景。</p>
+    <p><strong>my-reoute-history</strong>是<a href="https://github.com/laden666666/my-router">my-reoute</a>的一个子项目，旨在用于浏览器上实现高仿APP式的单页面路由。<strong>my-reoute-history</strong>屏蔽了浏览器的诸多行为：记录用户历史记录、前进按钮等，并监听用户返回动作，使得WebApp路由变得更像App。可用于PWA路由开发、WebApp路由开发、微信小程序路由移植等场景。</p>
 
     <h2>主要功能</h2>
 
--- packages/my-router-history/readme.md
+++ packages/my-router-history/readme.md
@@ -1,7 +1,7 @@
 
 # my-reoute-history
 
-**my-reoute-history**是[my-reoute](https://github.com/laden666666 "") 的一个子项目，旨在用于浏览器上实现高仿APP式的单页面路由。**my-reoute-history**屏蔽了浏览器的诸多行为：记录用户历史记录、前进按钮等，并监听用户返回动作，使得WebApp路由变得更像App。可用于PWA路由开发、WebApp路由开发、微信小程序路由移植等场景。
+**my-reoute-history**是[my-reoute](https://github.com/laden666666/my-router "") 的一个子项目，旨在用于浏览器上实现高仿APP式的单页面路由。**my-reoute-history**屏蔽了浏览器的诸多行为：记录用户历史记录、前进按钮等，并监听用户返回动作，使得WebApp路由变得更像App。可用于PWA路由开发、WebApp路由开发、微信小程序路由移植等场景。
 
 
 ## 主要功能
