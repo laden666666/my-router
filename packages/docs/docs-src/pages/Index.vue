bb9edc58b3102433acfe41f3d78768ac2304aab3
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2019-02-17 22:55:02 +0800

    提交history的文档

--- packages/docs/docs-src/pages/Index.vue
+++ packages/docs/docs-src/pages/Index.vue
@@ -1,14 +1,7 @@
 <template>
     <Doc>
-        <H1>test</H1>
-        <Browser ref="Browser1">
-            <Button @click="goTest">跳转到test</Button>
-            my栈：
-            
-            <pre 
-                v-for="(page, index) in pages" :key="index">第{{index + 1}}页:
-{{JSON.stringify(page, null, 4)}}</pre>
-        </Browser>
+        <H1>my-router</H1>
+        <P>在服务器端，路由是根据不同的Url路由，拼接出对应的视图页面技术。前后分离后，路由概念慢慢渗透到前端，现在连Reac Native、flutter、微信小程序等App框架也引入了路由的概念。</P>
     </Doc>
 </template>
 <script>
@@ -20,18 +13,5 @@ export default {
             pages: []
         }
     },
-    mounted(){
-        this.myHistory = window.myHistory = new MyHistory({}, this.$refs.Browser1.window)
-        this.pages = JSON.parse(JSON.stringify( this.myHistory.stack))
-
-        this.myHistory.onChange = ()=>{
-            this.pages = JSON.parse(JSON.stringify( this.myHistory.stack))
-        }
-    },
-    methods: {
-        async goTest(){
-            await this.myHistory.push('/test')
-        }
-    }
 }
 </script>
